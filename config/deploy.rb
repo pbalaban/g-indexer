4be5e9770932435f937ae9108f35f4f3eebffe05
Petr Balaban <balaban.petr@gmail.com>
2015-02-09 00:00:56 +0200

    Capistrano configurations.

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+lock '3.3.5'
+
+EXTRA_LINKED_DIRS  = %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+EXTRA_LINKED_FILES = %w{config/secrets.yml config/database.yml}
+
+set :application, 'g-indexer'
+set :repo_url, 'git@github.com:pbalaban/g-indexer.git'
+
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+set :linked_files, fetch(:linked_files, []).push(*EXTRA_LINKED_FILES)
+set :linked_dirs, fetch(:linked_dirs, []).push(*EXTRA_LINKED_DIRS)
+
+set :sidekiq_default_hooks, -> { false }
+
+after 'deploy:publishing', 'unicorn:legacy_restart'
+after 'deploy:finished', 'unicorn:restart'
---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+server '54.191.19.139',
+  user: 'ubuntu',
+  roles: %w{web app db},
+  ssh_options: {
+    keys: %w(~/.ssh/aws-pbalaban.pem),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+  }
+
+set :linked_files, fetch(:linked_files, []).push('config/settings/production.yml')
