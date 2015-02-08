53901979d631c617bca203dce5bfbdeedfc776e3
Petr Balaban <balaban.petr@gmail.com>
2015-02-08 23:09:27 +0200

    Remove :indexed field from Domain. Use dynamic method, based on :g_index.

--- db/migrate/20150129205724_create_domains.rb
+++ db/migrate/20150129205724_create_domains.rb
@@ -2,7 +2,6 @@ class CreateDomains < ActiveRecord::Migration
   def change
     create_table :domains do |t|
       t.string   :url
-      t.boolean  :indexed, default: false, null: false
       t.integer  :g_index, default: 0, null: false
       t.datetime :checked_at
 
