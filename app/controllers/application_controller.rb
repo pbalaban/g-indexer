c9f48b71ed3f7d5057c7aa64367548607ffcad6e
Petr Balaban <pbalaban@productengine.com>
2015-02-11 16:04:04 +0200

    Add basic_auth checking.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate
+
+  protected
+  def authenticate
+    return unless Settings.basic_auth.enabled
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Settings.basic_auth.login && password == Settings.basic_auth.password
+    end
+  end
 end
