04ed49f22dcff19e60575225971c87ed79da5039
Petr Balaban <pbalaban@productengine.com>
2015-02-17 17:47:07 +0200

    Use EmailAddress from DB for sending emails.

--- app/mailers/domain_mailer.rb
+++ app/mailers/domain_mailer.rb
@@ -2,12 +2,12 @@ class DomainMailer < ApplicationMailer
   def index_enabled(domain_id)
     @domain = Domain.find(domain_id)
 
-    mail(to: Settings.mailer.mail_to, subject: "Domain: #{@domain.url} is now indexed")
+    mail(to: EmailAddress.pluck(:email), subject: "Domain: #{@domain.url} is now indexed")
   end
 
   def index_disabled(domain_id)
     @domain = Domain.find(domain_id)
 
-    mail(to: Settings.mailer.mail_to, subject: "Domain: #{@domain.url} is now not-indexed")
+    mail(to: EmailAddress.pluck(:email), subject: "Domain: #{@domain.url} is now not-indexed")
   end
 end
