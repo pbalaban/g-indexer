d9e9abed87fe7db7c498783b53726d394f915e83
Petr Balaban <balaban.petr@gmail.com>
2016-08-27 13:18:04 +0300

    Extract cron job to class method.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -5,6 +5,21 @@ class Domain < ActiveRecord::Base
 
   validates :url, url: { allow_blank: true }, presence: true, uniqueness: true
 
+  def self.schedule_jobs
+    max_seconds   = (4.hours - 30.minutes).to_i
+    start_seconds = 30.minutes.to_i
+    seconds_step  = max_seconds/Domain.count
+
+    Domain.all.each.with_index do |domain, index|
+      range = Range.new(
+        start_seconds + index*seconds_step,
+        start_seconds + (index+1)*seconds_step
+      )
+
+      CheckHostJob.set(wait: rand(range).seconds).perform_later(domain)
+    end
+  end
+
   def host
     URI.parse(self.url).host
   end
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -1,13 +1,6 @@
 namespace :cron do
   desc 'Task which executed every 4 hours'
   task every_4_hours: :environment do
-    max_seconds   = (4.hours - 30.minutes).to_i
-    start_seconds = 30.minutes.to_i
-    seconds_step  = max_seconds/Domain.count
-
-    Domain.all.each.with_index do |domain, index|
-      range = Range.new(start_seconds + index*seconds_step, start_seconds + (index+1)*seconds_step)
-      CheckHostJob.set(wait: rand(range).seconds).perform_later(domain)
-    end
+    Domain.schedule_jobs
   end
 end
